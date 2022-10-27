a341aab0163e52ea5cefe2096612dd012e22f6ee
koalter <heavens_drive090@hotmail.com>
2022-10-27 18:54:38 -0300

    Configuracion Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
     projectId: 'clinica-online-de2f5',
     appId: '1:334916706368:web:0009f1510b0ae94ee43fa5',
     storageBucket: 'clinica-online-de2f5.appspot.com',
+    locationId: 'southamerica-east1',
     apiKey: 'AIzaSyBoR3V1nodpxbjp63j2q2RjLiaDyZyxB0k',
     authDomain: 'clinica-online-de2f5.firebaseapp.com',
     messagingSenderId: '334916706368',
