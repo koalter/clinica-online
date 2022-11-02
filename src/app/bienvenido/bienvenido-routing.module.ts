5c30f5c3c0645380cff4e3e8a7cb33545507b567
koalter <heavens_drive090@hotmail.com>
2022-11-02 00:45:15 -0300

    Seleccion de registro

--- src/app/bienvenido/bienvenido-routing.module.ts
+++ src/app/bienvenido/bienvenido-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroEspecialistaComponent } from './components/registro-especialista/registro-especialista.component';
 import { RegistroPacienteComponent } from './components/registro-paciente/registro-paciente.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registro/paciente', component: RegistroPacienteComponent },
   { path: 'registro/especialista', component: RegistroEspecialistaComponent },
-  { path: 'registro', redirectTo: 'registro/paciente', pathMatch: 'full' },
+  { path: 'registro', component: RegistroComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
